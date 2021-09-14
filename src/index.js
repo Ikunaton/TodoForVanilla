e2ed5a729dd21d08d3001f6ac2975d3b767fd069
Ikunaton <89513456+Ikunaton@users.noreply.github.com>
2021-09-14 16:12:45 +0900

    todo3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div
+  const div = document.createElement("div");
+  div.className = "list-row";
+  // li
+  const li = document.createElement("li");
+  li.innerText = inputText;
+  // button
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("ああ");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // div配下を消したい.
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+  //
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
